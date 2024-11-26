32d4b60fe293fc0012aa1a9356c52217fd19a3f7
maximKAw <168087872+maximKAw@users.noreply.github.com>
2024-11-26 17:10:06 +0900

    Initial commit

---
+++ Dla rasnosti/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class AbsoluteDifference
+{
+    public static int CalculateDifference(int n)
+    {
+        int difference = Math.Abs(n - 123);
+        if (n > 123)
+        {
+            difference *= 3;
+        }
+        return difference;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите целое число n:");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int n))
+        {
+            int result = CalculateDifference(n);
+            Console.WriteLine($"Абсолютная разность между {n} и 123: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Введите целое число.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Dla-rasnosti
+ 
